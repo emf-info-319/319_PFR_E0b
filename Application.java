f4163eb7049f51d65cb5cdd09b844657e5e81c5c
Paul Friedli <paul.friedli@edufr.ch>
2026-08-27 17:43:33 +0200

    Initial commit

---
+++ Application.java
@@ -0,0 +1,38 @@
+public class Application {
+
+    // -----------------------------------------------------------
+    // Sujets : constantes, tableaux, boucles light (+sujets E0a)
+    // -----------------------------------------------------------
+
+    public static void main(String[] args) {
+
+        // Au bon endroit, déclarez une constante qui représente l'âge de la retraite et
+        // donnez-lui la bonne valeur (65).
+
+        // Au bon endroit, déclarez une constante qui représente le nombre pi et
+        // donnez-lui la bonne valeur (3.1415926).
+
+        // Déclarez un tableau dans lequel on pourrait mettre 12 notes modulaires (mais
+        // ne le créez pas encore !).
+
+        // À droite de ce commentaire, mettez la réponse à la question.
+        // Que contient ce tableau ?? VOTRE REPONSE ICI
+
+        // Créez ce tableau à la taille demandée (12 notes modulaires)
+
+        // Affichez le contenu de la 2ème case de ce tableau
+
+        // Affichez la taille de ce tableau
+
+        // Modifier le contenu de la 5ème cellule du tableau en lui rajoutant 3
+
+        // Afficher le contenu de tout le tableau sous la forme :
+        // .
+        // .
+        // Cellule[3] contient [12]
+        // Cellule[4] contient [2]
+        // .
+
+    }
+
+}
